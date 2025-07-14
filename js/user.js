c97c5686369a9f982269d03b1965e03e5e3d8daa
Barzin <r66224950@gmail.com>
2025-07-14 13:55:27 +0330

    project1-part8

--- js/user.js
+++ js/user.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
 });
+
+function showModel() {
+    document.getElementById("model").classList.toggle("show");
+    document.getElementById("model_back").classList.toggle("dis-none");
+}
+
+document.getElementById("add_user").addEventListener('click', showModel);
+document.getElementById("submit").addEventListener('click', showModel);
